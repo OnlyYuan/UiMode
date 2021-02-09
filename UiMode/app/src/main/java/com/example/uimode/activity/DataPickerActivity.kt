ed7cc428b73aff8d1e4979041be89500ac08e05f
caipengfei <caipengfei92@qq.com>
2021-02-09 16:53:40 +0800

    dataPicker

--- UiMode/app/src/main/java/com/example/uimode/activity/DataPickerActivity.kt
+++ UiMode/app/src/main/java/com/example/uimode/activity/DataPickerActivity.kt
@@ -2,6 +2,7 @@ package com.example.uimode.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.uimode.R
 
 class DataPickerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
